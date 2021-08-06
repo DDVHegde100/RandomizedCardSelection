d5ecdb18d1223bd81c9512f88eeaf8c08a56b62e
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-06 10:40:10 -0400

    Add files via upload

---
+++ main (54).py
@@ -0,0 +1,10 @@
+import random
+cards = ["Diamonds", "Spades", "Hearts", "Clubs"]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+
+def cardr():
+    card = random.choices(cards)
+    rank = random.choices(ranks)
+    return("The {rank} of {card}")
+
+print(cardr())
